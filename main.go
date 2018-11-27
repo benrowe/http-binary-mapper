65b7b64e0edbd3f1e6fcf7a3d7951385873ef6ae
Ben Rowe <ben.rowe.83@gmail.com>
2018-11-28 07:52:08 +1100

    Add http/config logic

--- main.go
+++ main.go
@@ -2,8 +2,52 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	fmt.Print("reverse-proxy-binary-mapper")
+	loadConfig()
+	startServer()
+}
+
+func loadConfig() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yaml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s", err))
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+	})
+}
+
+func startServer() {
+	port := viper.GetInt32("config.http.port")
+	fmt.Println("booting server on port", port)
+
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/{slug}", handleMap).Methods("GET")
+
+	http.Handle("/", rtr)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
+
+func handleMap(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	name := params["slug"]
+	fmt.Println("incoming request: ", name)
+	viper.UnmarshalKey()
+	w.Write([]byte("test"))
 }
