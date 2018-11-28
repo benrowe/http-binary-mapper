f7b57970df14a8f7ef67e6725ebeb4a3dadcc754
Ben Rowe <ben.rowe.83@gmail.com>
2018-11-29 07:50:47 +1100

    Add binary call logic

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"os/exec"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/gorilla/mux"
 	"github.com/spf13/viper"
 )
 
@@ -22,7 +22,18 @@ type Config struct {
 
 // Map
 type Map struct {
-	ID string `mapstructure:"id"`
+	ID       string `mapstructure:"id"`
+	Token    string
+	Binaries []Bin
+}
+
+type Bin struct {
+	Name    string
+	Handler string
+}
+
+func (m Map) isValid(token string) bool {
+	return m.Token == token
 }
 
 var config Config
@@ -42,19 +53,21 @@ func loadConfig() {
 		viper.Unmarshal(&config)
 	})
 	viper.Unmarshal(&config)
-	fmt.Println("config read")
+	fmt.Println("Config: read")
 }
 
 func startServer() {
 	port := viper.GetInt32("config.http.port")
-	fmt.Println("booting server on port", port)
+	fmt.Println("Server: booting on port", port)
 
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/{slug}", handleMap).Methods("GET")
 
 	http.Handle("/", rtr)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	portString := ":" + viper.GetString("config.http.port")
+
+	if err := http.ListenAndServe(portString, nil); err != nil {
 		panic(err)
 	}
 }
@@ -62,17 +75,37 @@ func startServer() {
 func handleMap(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	name := params["slug"]
-	found := false
 
-	fmt.Println("incoming request: ", name)
-
-	for _, element := range config.Maps {
-		if element.ID == name {
-			found = true
+	fmt.Println("Request: handling: ", name)
+	m := findMap(name, config.Maps)
+	if len(m.ID) > 0 {
+		token, ok := r.URL.Query()["token"]
+		if ok && m.isValid(token[0]) {
+			go func() {
+				// call binaries
+				for _, bin := range m.Binaries {
+					fmt.Println(bin.Name)
+					exe := exec.Command(bin.Handler)
+					err := exe.Run()
+					fmt.Println(err)
+				}
+			}()
+		} else {
+			fmt.Println("Request: rejected ", name, "(403)")
+			w.WriteHeader(http.StatusForbidden)
 		}
-	}
-	if found == false {
-		fmt.Println("rejected: ", name)
+
+	} else {
+		fmt.Println("Request: rejected: ", name, "(404)")
 		w.WriteHeader(http.StatusNotFound)
 	}
+}
+
+func findMap(name string, maps []Map) Map {
+	for _, element := range maps {
+		if element.ID == name {
+			return element
+		}
+	}
+	return Map{}
 }
